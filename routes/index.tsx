bab1bae4e50567b00872d718765c1b180322bde1
czq040902 <16615812538@163.com>
2025-11-18 22:24:36 +0800

    确定样式

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,32 +1,57 @@
-import { useSignal } from "@preact/signals";
 import { Head } from "fresh/runtime";
 import { define } from "../utils.ts";
-import Counter from "../islands/Counter.tsx";
 
-export default define.page(function Home(ctx) {
-  const count = useSignal(3);
-
-  console.log("Shared value " + ctx.state.shared);
-
+export default define.page(function Home() {
   return (
     <div class="px-4 py-8 mx-auto fresh-gradient min-h-screen">
       <Head>
         <title>Fresh counter</title>
       </Head>
-      <div class="max-w-3xl mx-auto flex flex-col items-center justify-center">
-        <img
-          class="my-6"
-          src="/logo.svg"
-          width="128"
-          height="128"
-          alt="the Fresh logo: a sliced lemon dripping with juice"
-        />
-        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
-        <p class="my-4">
-          Try updating this message in the
-          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
-        </p>
-        <Counter count={count} />
+      <div class="max-w-3xl mx-auto pt-32">
+        <table>
+          <caption>
+            December 2025 / 2025 年 12 月
+          </caption>
+          <thead>
+            <tr>
+              <th scope="col" className="break-keep">作者</th>
+              <th scope="col">标题</th>
+              <th scope="col">日期</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">阮一峰</th>
+              <td className="jc">
+                具有 TypeScript
+                端到端的类型安全，统一的类型系统和出色的开发人员体验。由 Bun
+                提供加速支持。
+              </td>
+              <td>22</td>
+            </tr>
+            <tr>
+              <th scope="row">-</th>
+              <td>Web accessibility</td>
+              <td>25</td>
+            </tr>
+            <tr>
+              <th scope="row">土木坛子</th>
+              <td>JavaScript frameworks</td>
+              <td>18</td>
+            </tr>
+            <tr>
+              <th scope="row">Karen</th>
+              <td>具有 TypeScript 验。由 Bun 提供加速支持。</td>
+              <td>15</td>
+            </tr>
+          </tbody>
+          <tfoot>
+            <tr>
+              <th scope="row" colspan={2}>本月总发布数量</th>
+              <td>8 篇</td>
+            </tr>
+          </tfoot>
+        </table>
       </div>
     </div>
   );
